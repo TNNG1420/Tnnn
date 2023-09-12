5db471fd429ba4d2e2c4b74bad6765536f77066c
TanBD <tanbd@rikkeisoft.com>
2023-09-12 13:57:02 +0700

    change

---
+++ app/src/main/java/com/example/diaryapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.diaryapp.utils
+
+object Constants {
+
+    const val APP_ID = "application-0-hmlpv"
+    const val CLIENT_ID = "325323446202-12hfk36lh7c0b1pp5t13lihd62tg6qol.apps.googleusercontent.com"
+}
